b20899b353de330dbbca94fa123c395d2bc89ceb
Eduardo Matos <eduardo.matos@priceline.com>
2022-04-12 21:58:54 -0300

    Initial commit

---
+++ src/main/java/com/ematos/mygroce/MygroceApplication.java
@@ -0,0 +1,13 @@
+package com.ematos.mygroce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygroceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygroceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ematos/mygroce/MygroceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ematos.mygroce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygroceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
